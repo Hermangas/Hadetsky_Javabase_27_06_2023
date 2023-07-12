b5bcec0cbe58c5aaba8c1d47c4fa89d0b626e616
Hadetskyi Herman <gadetsky13@gmal.com>
2023-07-12 23:05:48 +0200

    Hello world

---
+++ src/main/java/ua/hillel/Hadetskyi/lessons/lesson4/LiAndMin.java
@@ -0,0 +1,28 @@
+package ua.hillel.Hadetskyi.lessons.lesson4;
+
+public class LiAndMin {
+    public static void main(String[] args) {
+
+        int warriorLin = 13;
+        int shooterLin = 24;
+        int horseriderLin = 46;
+
+        int linAmount = 860;
+
+        int linAttack = (warriorLin + shooterLin + horseriderLin) * linAmount;
+
+        int warriorMin = 9;
+        int shooterMin = 35;
+        int horseriderMin = 12;
+
+        double advantage = 1.5 * linAmount;
+
+        int minAttack = (int) ((warriorMin + shooterMin + horseriderMin) * advantage);
+
+
+        System.out.println("Загальна атака Лі: " + linAttack );
+        System.out.println("Загальна атака Мінь: " + minAttack);
+
+    }
+
+}
